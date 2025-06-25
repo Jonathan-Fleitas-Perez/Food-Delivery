25865547e3f649b225b8e918ff6c148c1d151b9c
Jonathan-Fleitas-Perez <jonathanfleitasperez@gmail.com>
2025-06-25 01:19:09 -0400

    cors2

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,19 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  req.url = req.url.replace(/\/{2,}/g, '/');
+  next();
+});
 // Middlewares
 app.use(express.json());
 app.use(cors(corsOptions));
